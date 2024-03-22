1552aaa83c0bebe70b4560164c461162b70b043c
JamesZam96 <jameszamgm@gmail.com>
2024-03-22 15:04:29 -0500

    botones

---
+++ Botones/app/src/main/java/com/sena/buttons/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.sena.buttons
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val toast = Toast.makeText(this,"Hola",Toast.LENGTH_LONG)
+
+        val buttonToast = findViewById<Button>(R.id.btnGoToButtons)
+        buttonToast.setOnClickListener {
+            val intent = Intent(this,Buttons::class.java)
+            toast.show()
+            startActivity(intent)
+        }
+
+
+    }
+}
